15c92a33751fd0d835f4d764351dabad94d311ec
Andrew Yu <andrew.yu6@hotmail.com>
2018-01-06 11:50:56 +1100

    Update recon.py

--- recon.py
+++ recon.py
@@ -3,7 +3,7 @@
 from multiprocessing import Process
 import subprocess
 import os 
-from ftplib import FTP
+import argparse 
 
 jobs = [] # love me some multiprocessing 
 
@@ -15,21 +15,38 @@ class colors:
     	ENDC = "\033[0m"
     	BOLD = "\033[1m"
 
-def metasploit(command):
+def snmp_check(ip, port, out):
+	print colors.INFO + "Starting snmp_check on %s:%s" % (ip, port) + colors.ENDC 
+	communitystrings = ["public","private","community"]
+	for string in communitystrings:
+		snmpscan = "snmp-check -t %s -p %s -c %s" % (ip, port, string)
+		results = subprocess.check_output(snmpscan, shell=True)
+		
+		f = open(os.path.join(ip, "%s:%s.snmp_check.txt" % (ip,port)), "w")
+		f.write(results)
+		f.close()
+
+def metasploit(command, out):
 	print colors.INFO + "Starting metasploit with command %s" % (command) + colors.ENDC
-	msfscan = "msfconsole -q -x '%s'" % (command)
+	msfscan = "msfconsole -q -x '%s; quit'" % (command)
 	results = subprocess.check_output(msfscan, shell=True)
+	f = open(out+".mssqlping.txt","w")
+	f.write(results)
+	f.close()
+
 def enum4linux(ip):
 	print colors.INFO + "Starting enum4linux on host %s" % (ip) + colors.ENDC
 	enumscan = "enum4linux -a %s" % (ip)
 	results = subprocess.check_output(enumscan, shell=True)
 
-def nmap_scan(ip, ports, scripts):
+def nmap_scan(ip, ports, scripts, serv, out, outtag):
 	ports = [x.split("/")[0] for x in ports]
-	ports = ",".join(ports)
+	portlist = ",".join(ports)
 	print colors.INFO + "Starting NMAP on host %s with scripts %s" % (ip, scripts) + colors.ENDC
-	nmapscan = "nmap --script=%s -p %s %s" % (scripts, ports, ip)
+	nmapscan = "nmap --script=%s -p %s %s -oN %s" % (scripts, portlist, ip, os.path.join(out,"%s:%s.nmapscan" % (ip,outtag)))
+	print nmapscan
 	results = subprocess.check_output(nmapscan,shell=True)
+	
 
 def gobuster_scan(ip, ports, wordlist, extensions, serv, out):
 	ports = [x.split("/")[0] for x in ports]	
@@ -37,7 +54,7 @@ def gobuster_scan(ip, ports, wordlist, extensions, serv, out):
 		print colors.INFO + "Starting Gobuster Scan on Port: %s" % (port) 
 		print "Service: %s" % (serv) + colors.ENDC
 		# hack mcgee #2 
-		if "ssl" in serv:
+		if "ssl" in serv or "https" in serv:
 			scheme = "https://"
 		else:
 			scheme = "http://"
@@ -47,26 +64,26 @@ def gobuster_scan(ip, ports, wordlist, extensions, serv, out):
 		
 def nikto_scan(ip, ports, serv, out):
 	ports = [x.split("/")[0] for x in ports]
-	ports = ",".join(ports)
-	print colors.INFO + "Starting Nikto Scan On Ports: %s" % (ports)
+	portlist = ",".join(ports)
+	print colors.INFO + "Starting Nikto Scan On Ports: %s" % (portlist)
 	print "Service: %s" % (serv) + colors.ENDC 
-	niktoscan = "nikto -ask no -h %s -p %s -F htm -output %s" % (ip, ports, os.path.join(out, "%s:%s.niktoscan.html" % (ip, serv.replace("/",""))))
+	niktoscan = "nikto -ask no -h %s -p %s -F htm -output %s" % (ip, portlist, out+"%s:%s.niktoscan.html" % (ip, ports[0]))
 	print niktoscan
 	results = subprocess.check_output(niktoscan, shell=True) #Suppress normal output
 
-def service_scan(ip, out, intensive=False, wordlist="/usr/share/wordlists/dirb/common.txt", extensions=""):
+def service_scan(ip, out, aggressive=False, wordlist="/usr/share/wordlists/dirb/common.txt", extensions=""):
 	ip = ip.strip() 
 	results = ""
-	if not intensive:
+	if not aggressive:
 		print colors.INFO + "Initial NMAP Scan For " + ip + colors.ENDC
-		scan = "nmap -sC -sV %s -oA '%s/%s.initialnmap'" % (ip, out, ip)
+		scan = "nmap -sC -sV %s -oA '%s.initialnmap'" % (ip, os.path.join(out,ip))
 		results = subprocess.check_output(scan, shell=True)	
 		print colors.GREEN + "Results For " + ip + colors.ENDC
 		print results 
 	else:
 		print colors.INFO + "Starting Intensive NMAP TCP and UDP Scans For %s - Might take a while" % (ip) + colors.ENDC
-		tcpscan = "nmap -v -Pn -sS -A -sC -p- -T 3 -script-args=unsafe=1 %s -oA '%s/%s.tcpnmap'"  % (ip, out, ip)
-		udpscan = "nmap -v -sC -sV -sU %s -oA '%s/%s.udpnmap'" % (ip, out,ip)
+		tcpscan = "nmap -v -Pn -sS -A -sC -p- -T 3 -script-args=unsafe=1 %s -oA '%s.tcpnmap'"  % (ip, os.path.join(out,ip))
+		udpscan = "nmap -v -sC -sV -sU %s -oA '%s.udpnmap'" % (ip, os.path.join(out,ip))
 
 		tcpresults = subprocess.check_output(tcpscan, shell=True)
 		print colors.GREEN + "TCP Results For " + ip + colors.ENDC
@@ -76,6 +93,8 @@ def service_scan(ip, out, intensive=False, wordlist="/usr/share/wordlists/dirb/c
 		print udpresults 
 		results = tcpresults + udpresults  #2lazy2change slow string results
 	serv_dict = {}
+	# Open Recommendation File 
+	recommendations = open("%s.recommendations.txt" % (os.path.join(out,ip)),"w")
 	lines = results.split("\n")
 	for line in lines:
 		ports = []
@@ -104,6 +123,9 @@ def service_scan(ip, out, intensive=False, wordlist="/usr/share/wordlists/dirb/c
 		#Launch Additional Scans
  		if "ftp" in serv:
 			print "[*] FTP Service detected"
+			if "2100" in [x.split("/")[0] for x in ports]:
+				print colors.GOOD + "Possible ORACLE XML DB FTP" + colors.ENDC
+				recommendations.write("- Possible ORACLE XML DB with FTP access on port 2100. Check https://docs.oracle.com/cd/B10501_01/win.920/a95490/username.htm for a list of logins.") 
 		elif serv == "http" or serv == "ssl/http" or "https" in serv or "http" in serv:
 			print "[*] Web service detected - Launching Nikto and Gobuster" 
 			p = Process(target=nikto_scan, args=(ip, ports, serv, out))
@@ -117,33 +139,50 @@ def service_scan(ip, out, intensive=False, wordlist="/usr/share/wordlists/dirb/c
 			print "[*] MySQL service detected"
 		elif "microsoft-ds" in serv:
 			print "[*] Microsoft Directory Service detected - Launching NMAP SMB Vuln Scan and enum4linux"
-			p = Process(target=nmap_scan, args=(ip, ports, "smb-enum-shares.nse,smb-ls.nse,smb-enum-users.nse,smb-mbenum.nse,smb-os-discovery.nse,smb-security-mode.nse,smbv2-enabled.nse,smb-vuln-cve2009-3103.nse,smb-vuln-ms06-025.nse,smb-vuln-ms07-029.nse,smb-vuln-ms08-067.nse,smb-vuln-ms10-054.nse,smb-vuln-ms10-061.nse,smb-vuln-regsvc-dos.nse,smbv2-enabled.nse"))
+			p = Process(target=nmap_scan, args=(ip, ports, "smb-enum-shares,smb-ls,smb-enum-users,smb-mbenum,smb-os-discovery,smb-security-mode,smb-vuln-cve2009-3103,smb-vuln-ms06-025,smb-vuln-ms07-029,smb-vuln-ms08-067,smb-vuln-ms10-054,smb-vuln-ms10-061,smb-vuln-regsvc-dos,", serv, out,"smbvuln"))
 			jobs.append(p)
 			p.start()
 
-			p = Process(target=enum4linux, args=(ip))
+			p = Process(target=enum4linux, args=(ip,))
 			jobs.append(p)
 			p.start()
 		elif "ms-sql" in serv:
-			print "[*] Microsoft SQL detected - Launching Metasploit Scan" 
-			p = Process(target=metasploit, args=("use auxiliary/scanner/mssql/mssql_ping; set RHOSTS %s; run" % (ip)))
-			jobs.append(p)
-			p.start()
-		elif "msdrdp" in serv or "ms-wbt-server" in serv or "microsoft-rdp" in serv:
-			print "[*] Microsoft Remote Desktop Protocol service detected"
-		elif "msrpc" in serv:
-			print "[*] Microsoft Remote Procedure Call service detected - Launching NMAP Enumeration Scan"
-			p = Process(target=nmap_scan, args=(ip, ports, "msrpc-enum"))
+			print "[*] Microsoft SQL detected - Launching Metasploit Confirmation Ping"
+			print colors.GOOD + "Recommendation: Try Default Login with 'sa:' and 'sa:Password123'" + colors.ENDC 
+			p = Process(target=metasploit, args=("use auxiliary/scanner/mssql/mssql_ping; set RHOSTS %s; run" % (ip), out+ip))
 			jobs.append(p)
 			p.start()
 			
+		elif "msdrdp" in serv or "ms-wbt-server" in serv or "microsoft-rdp" in serv:
+			print "[*] Microsoft Remote Desktop Protocol service detected - Launching NMAP Vuln Check"
+			p = Process(target=nmap_scan, args=(ip, ports, "rdp-vuln-ms12-020", serv, out,"rdpvuln"))
+			jobs.append(p)
+			p.start()
+		elif "msrpc" in serv:
+			print "[*] Microsoft Remote Procedure Call service detected - Launching NMAP Enumeration Scan"
+			p = Process(target=nmap_scan, args=(ip, ports, "msrpc-enum", serv, out,"rpcenum"))
+			jobs.append(p)
+			p.start()
+		elif "mysql" in serv:
+			print "[*] MySQL Service Discovered - Launching NMAP Scripts"
+			p = Process(target=nmap_scan, args=(ip, ports, "mysql-audit,mysql-databases,mysql-dump-hashes,mysql-empty-password,mysql-enum,mysql-info,mysql-query,mysql-users,mysql-variables,mysql-vuln-cve2012-2122", serv, out,"mysqlvulnenum"))
+			jobs.append(p)
+			p.start() 
 		elif "smtp" in serv:
 			print "[*] SMTP service detected - Launching NMAP Vuln Checks"
-			p = Process(target=nmap_scan, args=(ip, ports, "smtp-commands,smtp-enum-users,smtp-vuln-cve2010-4344,smtp-vuln-cve2011-1720,smtp-vuln-cve2011-1764"))
+			p = Process(target=nmap_scan, args=(ip, ports, "smtp-commands,smtp-enum-users,smtp-vuln-cve2010-4344,smtp-vuln-cve2011-1720,smtp-vuln-cve2011-1764", serv, out,"smtpvulnenum"))
 			jobs.append(p)
 			p.start()
 		elif "snmp" in serv:
-			print "[*] SNMP service detected"
+			print "[*] SNMP service detected - Launching NMAP Enum Scripts and SNMP-Check"
+			p = Process(target=nmap_scan, args=(ip, ports, "snmp-netstat,snmp-processes", serv, out,"snmpenum"))
+			jobs.append(p)
+			p.start()
+			for port in [x.split("/")[0] for x in ports]:
+				p = Process(target=snmp_check, args=(ip, port, out))
+				jobs.append(p)
+				p.start()
+			
 		elif "ssh" in serv: 
 			print "[*] SSH service detected"
 		elif "telnet" in serv:
@@ -152,17 +191,31 @@ def service_scan(ip, out, intensive=False, wordlist="/usr/share/wordlists/dirb/c
 		#Interesting Services 
 		if "squid" in serv:
 			print colors.GOOD + "SQUID Proxy Detected!" + colors.ENDC
-		
-		print "\n"
-
+			recommendations.write("- SquidProxy detected, see if you can pivot to internal services. (Conduct an NMAP Scan using the squidproxy as a proxy...)")
+		#print "\n"	
 	f.close()
+	recommendations.close()
+
 	
 
+#main 
+parser = argparse.ArgumentParser(description="OSCP Recon Script") 
+parser.add_argument("target", help="target address")
+parser.add_argument("-o", "--output", help="output directory", required=True) 
+parser.add_argument("-w", "--wordlist", help="wordlist for directory discovery", required=False, default="/usr/share/wordlists/dirb/common.txt")
+parser.add_argument("-x", "--extensions", help="comma seperated extensions for directory discovery e.g. php, txt", required=False, default="php")  
+parser.add_argument("-a", "--aggressive", help="aggressive service scanning", required=False, action="store_true")
+
+args = parser.parse_args()
+
+if not os.path.isdir(args.output):
+	print colors.FAIL + "Supplied output directory not a directory" + colors.ENDC
+	raise SystemExit()
 
 
 
-#os check out is not a directory blah blah 
-service_scan("127.0.0.1", "/tmp/", False, "/usr/share/wordlists/dirb/common.txt", "php")
-	
-#for job in jobs:
-	#job.join()
+service_scan(args.target, args.output, args.aggressive, args.wordlist, args.extensions)
+for job in jobs:
+	job.join()
+
+
